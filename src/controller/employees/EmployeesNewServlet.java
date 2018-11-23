d32f73fcb533112178a9c638ee5df0a3406fc620
kenta.w <kenta.ctw@gmail.com>
2018-11-23 10:22:47 +0900

    Add Employee newx

--- src/controller/employees/EmployeesNewServlet.java
+++ src/controller/employees/EmployeesNewServlet.java
@@ -1,11 +1,15 @@
 package controller.employees;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
 
 /**
  * Servlet implementation class EmployeesNewServlet
@@ -13,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/employees/new")
 public class EmployeesNewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +30,12 @@ public class EmployeesNewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("_token", request.getSession().getId());
+		request.setAttribute("employee", new Employee());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+		rd.forward(request, response);
+
 	}
 
 }
